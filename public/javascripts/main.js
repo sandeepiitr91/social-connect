aacd2194553a9cb5b39075210d43d012ef518812
Avik Sarkar <aviks@datetheramp.com>
2016-08-24 19:21:38 +0530

    no message

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,7 +10,7 @@ app.config(function($httpProvider, $stateProvider, $urlRouterProvider, $location
   $stateProvider.state('root', {
       url: '/',
       template: '<div><pre>{{ctrl.response | json}}</pre></div>\
-        </div ng-if="ctrl.step == 1"><div><button><a href="http://www.linkedin.com/oauth/v2/authorization?client_id=816dleijgt4exs&response_type=code&redirect_uri=https://sleepy-wildwood-51219.herokuapp.com/&state=XXXA354S78D968ASDA789SD8567rg456ASD&scope=r_basicprofile">Start First Step</a></button></div>\
+        <div ng-if="ctrl.step == 1"><button><a href="http://www.linkedin.com/oauth/v2/authorization?client_id=816dleijgt4exs&response_type=code&redirect_uri=https://sleepy-wildwood-51219.herokuapp.com/&state=XXXA354S78D968ASDA789SD8567rg456ASD&scope=r_basicprofile">Start First Step</a></button></div>\
         </div><div ng-if="ctrl.step == 2"><button ng-click="ctrl.startSecondStep()">Start Second Step</button></div>\
       ',
       controller: 'MainCtrl',
